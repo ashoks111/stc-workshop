c59b64673e960269049bc9f0d4f1dd73b9746ccd
Ashok <ashok.p@diagnal.com>
2025-04-24 13:28:21 +0530

    revert padding

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
-export const MOVIE_CARD_PADDING_LENGTH = 24; // padding length for Movie Rail
+export const MOVIE_CARD_PADDING_LENGTH = 12; // padding length for Movie Rail
 /** Default URL for the user's avatar image. */
 export const USER_AVATAR = "https://images.unsplash.com/photo-1728577740843-5f29c7586afe?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 /** Maximum number of items to display per page in paginated lists. */
